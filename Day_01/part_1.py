61348287a26566f8c9368b8e3798ee9a6063663b
LindseyJorissen <Lindsey.jorissen@gmail.com>
2024-12-11 06:33:01 +0100

    Added comments

--- Day_01/part_1.py
+++ Day_01/part_1.py
@@ -1,4 +1,9 @@
+""" Advent of Code Day 1 - Pair up the smallest number in the left list with the smallest number in the right list.
+Within each pair, figure out how far apart the two numbers are
+and add up all of those distances."""
+
 def read_input_file(filename):
+    """read inputfile with puzzle input, return two lists. One for the left and one for the right column"""
     left_column = []
     right_column = []
     with open(filename, "r") as inputfile:
@@ -9,22 +14,22 @@ def read_input_file(filename):
     return left_column, right_column
 
 def calculate_differences(left_column, right_column):
+    """"Sort both input lists and return the sum of the difference between the nth numbers"""
     left_column.sort()
     right_column.sort()
-    difference = []
+    difference = 0
     for i in range(len(left_column)):
         if left_column[i] >= right_column[i]:
-            difference.append(left_column[i] - right_column[i])
+            difference += (left_column[i] - right_column[i])
         else:
-            difference.append(right_column[i] - left_column[i])
+            difference += (right_column[i] - left_column[i])
     return difference
 
 def main():
     input_file = "input.txt"
     left_column, right_column = read_input_file(input_file)
     difference = calculate_differences(left_column, right_column)
-    print(sum(difference))
+    print(f"Part 1 : {difference}")
 
-if __name__ == '__main__':
-    main()
+main()
     
--- Day_01/part_2.py
+++ Day_01/part_2.py
@@ -1,15 +1,22 @@
+"""Calculate a 'similarity score' by counting how often each number from the left list
+appears in the right list and summing the products of each number in the left list with
+its occurrence amount in the right list."""
 from part_1 import read_input_file
 
 def count_occurrence(left_column, right_column):
+    """read inputfile with puzzle input, and return a dictionary where keys are numbers from the left column
+    and values are the counts of their occurrences in the right column."""
     occurrence = {}
     for i in range(len(left_column)):
        occurrence.update({left_column[i]: right_column.count(left_column[i])})
     return occurrence
 
 def calc_similarity_score(occurrence):
-    similarity_score = []
+    """Calculate the 'similarity score' by summing the product of each number in the left
+    column and its occurrence count from the right column."""
+    similarity_score = 0
     for key, value in occurrence.items():
-        similarity_score.append(key * value)
+        similarity_score += (key * value)
     return similarity_score
 
 def main():
@@ -17,8 +24,7 @@ def main():
     left_column, right_column = read_input_file(input_file)
     occurrence = count_occurrence(left_column, right_column)
     similarity_score = calc_similarity_score(occurrence)
-    print(sum(similarity_score))
+    print(f"Part 2 : {similarity_score}")
 
-if __name__ == '__main__':
-    main()
+main()
 
