175ae748fa591e744b4b48692ad967f16c1ffc43
LindseyJorissen <Lindsey.jorissen@gmail.com>
2024-12-11 07:18:38 +0100

    Added documentation

--- Day_02/part_2.py
+++ Day_02/part_2.py
@@ -10,20 +10,43 @@ def can_become_safe_by_removing_one(unsafe_reports):
         found_safe = False
         for i in range(len(report)):
             modified_report = report[:i] + report[i + 1:]
-            if len(modified_report) > 1:
-                if ( modified_report == sorted(modified_report) and \
-                    all(1 <= modified_report[j + 1] - modified_report[j] <= 3 for j in range(len(modified_report) - 1))) or \
-                    modified_report == sorted(modified_report, reverse=True) and \
-                    all(1 <= modified_report[j] - modified_report[j + 1] <= 3 for j in range(len(modified_report) - 1)):
-                    updated_safe_reports.append(modified_report)
-                    found_safe = True
-                    break
+
+            if modified_report == sorted(modified_report):
+                sorted_ascending = True
+                for j in range(len(modified_report) - 1):
+                    if not (1 <= modified_report[j + 1] - modified_report[j] <= 3):
+                        sorted_ascending = False
+                        break
+
+                if sorted_ascending:
+                    is_safe = True
+                else:
+                    is_safe = False
+
+            elif modified_report == sorted(modified_report, reverse=True):
+                sorted_descending = True
+                for j in range(len(modified_report) - 1):
+                    if not (1 <= modified_report[j] - modified_report[j + 1] <= 3):
+                        sorted_descending = False
+                        break
+
+                if sorted_descending:
+                    is_safe = True
+                else:
+                    is_safe = False
+
+            else:
+                is_safe = False
+                
+            if is_safe:
+                updated_safe_reports.append(modified_report)
+                break
     return updated_safe_reports
 
 def main():
     filename = "input.txt"
     reports = read_input_file(filename)
-    unsafe_reports = is_safe(reports)
+    safe_reports,unsafe_reports = is_safe(reports)
     updated_safe_reports= can_become_safe_by_removing_one(unsafe_reports)
     print(f"Part 2: New amount of safe reports: {len(updated_safe_reports)}")
 
