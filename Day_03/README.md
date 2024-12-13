3c25ecc3e05091e07f506dbaf5e3267ffc44a420
LindseyJorissen <Lindsey.jorissen@gmail.com>
2024-12-13 23:01:44 +0100

    Added Day 3: Part 2 + Documentation

---
+++ Day_03/README.md
@@ -0,0 +1,8 @@
+For Day 1 and Day 2 I tried to use basic Python without imports, 
+mainly for practice but also because I'm a noob and I didn't want to go and learn 
+how to use a bunch of packages. 
+
+I managed to do this till Day 3 part 2, where I was stuck for 2 days...
+I caved and looked up regex since I'd seen many people use it for this puzzle. 
+
+Some documentation, for myself and other noobs attempting Advent of Code with very basic knowledge. 
---
+++ Day_03/part_2.py
@@ -0,0 +1,48 @@
+"""Advent of Code Day 3 - Look though inputfile and find mul(x,y) within corrupted data.
+Multiply X*Y and add result in a total, for part 2: start ignoring mul's when don't() is encountered"""
+import re
+def read_input_file():
+    """read inputfile with puzzle input and return a long string containing each line"""
+    with open("input.txt", "r") as inputfile:
+        return inputfile.read()
+
+def find_valid_mul(input):
+    """using regular expression (re) to find all mul(X,Y) patterns in string. Also do() and don't().
+    and returns a list of all matching patterns"""
+    return re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)",input)
+
+def find_multiplications(expressions):
+    """Goes through list of muls and do()/don't() statements and multiplies the ints"""
+    total = 0
+    for i in expressions:
+        if i.startswith("mul"):
+            nums = list(map(int, re.findall(r'\d{1,3}', i)))
+            total += nums[0] * nums[1]
+    return total
+
+def multiplications_if_do(expressions):
+    """Checks if next expression is a do() or don't() and multiply muls if True"""
+    total = 0
+    do_enabled = True
+    for mul in expressions:
+        if mul == "do()":
+            do_enabled = True
+        elif mul == "don't()":
+            do_enabled = False
+        elif do_enabled and mul.startswith("mul"):
+            nums = list(map(int, re.findall(r'\d{1,3}', mul)))
+            total += nums[0] * nums[1]
+    return total
+
+
+def main():
+    input = read_input_file()
+    expressions = find_valid_mul(input)
+
+    part1 = find_multiplications(expressions)
+    part2 = multiplications_if_do(expressions)
+
+    print(f"Part 1: {part1}\nPart 2: {part2}")
+
+
+main()
