3034960d4521a42e2d45fb159afcdbdad83988f7
LindseyJorissen <Lindsey.jorissen@gmail.com>
2024-12-13 23:40:02 +0100

    Added documentation

--- Day_03/part_2.py
+++ Day_03/part_2.py
@@ -1,9 +1,9 @@
 """Advent of Code Day 3 - Look though inputfile and find mul(x,y) within corrupted data.
 Multiply X*Y and add result in a total, for part 2: start ignoring mul's when don't() is encountered"""
 import re
-def read_input_file():
+def read_input_file(inputfile):
     """read inputfile with puzzle input and return a long string containing each line"""
-    with open("input.txt", "r") as inputfile:
+    with open(inputfile, "r") as inputfile:
         return inputfile.read()
 
 def find_valid_mul(input):
@@ -15,7 +15,7 @@ def find_multiplications(expressions):
     """Goes through list of muls and do()/don't() statements and multiplies the ints"""
     total = 0
     for i in expressions:
-        if i.startswith("mul"):
+        if i[0]=="m":
             nums = list(map(int, re.findall(r'\d{1,3}', i)))
             total += nums[0] * nums[1]
     return total
@@ -24,19 +24,20 @@ def multiplications_if_do(expressions):
     """Checks if next expression is a do() or don't() and multiply muls if True"""
     total = 0
     do_enabled = True
-    for mul in expressions:
-        if mul == "do()":
+    for i in expressions:
+        if i == "do()":
             do_enabled = True
-        elif mul == "don't()":
+        elif i == "don't()":
             do_enabled = False
-        elif do_enabled and mul.startswith("mul"):
-            nums = list(map(int, re.findall(r'\d{1,3}', mul)))
+        elif do_enabled and i.startswith("m"):
+            nums = list(map(int, re.findall(r'\d{1,3}', i)))
             total += nums[0] * nums[1]
     return total
 
 
 def main():
-    input = read_input_file()
+    inputfile = "input.txt"
+    input = read_input_file(inputfile)
     expressions = find_valid_mul(input)
 
     part1 = find_multiplications(expressions)
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Welcome to my Advent of Code 2024 repository! This repo contains my solutions an
 
 Advent of Code is an annual event where developers around the world solve coding puzzles every day throughout December, using their language of choice. Each day presents a new challenge, and I'll be trying to solve them using Python.
 
-Learn more about Advent of Code 2024 on their site [here](https://adventofcode.com/2024/about)
+Learn more about Advent of Code 2024 on their site [herhttps://adventofcode.com/2024/about)
 
 ## Project Structure
 
